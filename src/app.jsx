813d95c001f60bb223c1bf3f0b332c7d34f86c33
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-05 01:47:11 -0300

    ✔  Componente Preco criado

--- src/app.jsx
+++ src/app.jsx
@@ -14,17 +14,32 @@ const Home = () => {
 const Sobre = () => {
   return (
     <main className="main-about">
-    <section>
-      <div>
-        <h1>Sobre o ViajouAnotou.</h1>
-        <p>O ViajouAnotou nasceu do desejo dos amigos Paulo e Roberto de compartilharem de forma rápida suas aventuras pelo mundo.</p>
-        <p>Aos poucos, esse desejo virou realidade em forma de software entre amigos e familiares. Hoje, você também pode ser parte dessa comunidade.</p>
-      </div>
-      <img src="sobre-viajou-anotou.jpg" alt="Quatro pessoas sentadas admirando uma paisagem" />
-    </section>
-  </main>
+      <section>
+        <div>
+          <h1>Sobre o ViajouAnotou.</h1>
+          <p>O ViajouAnotou nasceu do desejo dos amigos Paulo e Roberto de compartilharem de forma rápida suas aventuras pelo mundo.</p>
+          <p>Aos poucos, esse desejo virou realidade em forma de software entre amigos e familiares. Hoje, você também pode ser parte dessa comunidade.</p>
+        </div>
+        <img src="sobre-viajou-anotou.jpg" alt="Quatro pessoas sentadas admirando uma paisagem" />
+      </section>
+    </main>
   )
 }
+
+const Preco = () => {
+  return (
+    <main className="main-pricing">
+      <section>
+        <div>
+          <h1>Preço simples.<br />Só R$ 47/mês.</h1>
+          <p>Comece hoje mesmo a anotar suas aventuras e mostre aos seus amigos o quê você fez pelo mundo.</p>
+        </div>
+        <img src="preco-viajou-anotou.jpg" alt="Pessoas transitando em uma faixa de pedestre" />
+      </section>
+    </main>
+  )
+}
+
 
 const App = () => {
   return (
