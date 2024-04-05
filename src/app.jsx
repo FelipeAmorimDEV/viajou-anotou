16a7bd6d0b624b1c2043a90ef5296fb1f26df5cc
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-05 01:54:29 -0300

    ✔  Rotas Criadas

--- src/app.jsx
+++ src/app.jsx
@@ -1,49 +1,7 @@
-
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom"
 const Home = () => {
   return (
-    <main className="main-home">
-      <section>
-        <h1>Você viaja o mundo. <br />E o ViajouAnotou mantém suas aventuras anotadas.</h1>
-        <h2>Um mapa mundial que rastreia por onde você passou. Nunca esqueça suas experiências e mostre aos seus amigos o quê você fez pelo mundo.</h2>
-        <a className="cta" href="">Começar agora</a>
-      </section>
-    </main>
-  )
-}
-
-const Sobre = () => {
-  return (
-    <main className="main-about">
-      <section>
-        <div>
-          <h1>Sobre o ViajouAnotou.</h1>
-          <p>O ViajouAnotou nasceu do desejo dos amigos Paulo e Roberto de compartilharem de forma rápida suas aventuras pelo mundo.</p>
-          <p>Aos poucos, esse desejo virou realidade em forma de software entre amigos e familiares. Hoje, você também pode ser parte dessa comunidade.</p>
-        </div>
-        <img src="sobre-viajou-anotou.jpg" alt="Quatro pessoas sentadas admirando uma paisagem" />
-      </section>
-    </main>
-  )
-}
-
-const Preco = () => {
-  return (
-    <main className="main-pricing">
-      <section>
-        <div>
-          <h1>Preço simples.<br />Só R$ 47/mês.</h1>
-          <p>Comece hoje mesmo a anotar suas aventuras e mostre aos seus amigos o quê você fez pelo mundo.</p>
-        </div>
-        <img src="preco-viajou-anotou.jpg" alt="Pessoas transitando em uma faixa de pedestre" />
-      </section>
-    </main>
-  )
-}
-
-
-const App = () => {
-  return (
-    <div className="app">
+    <>
       <header>
         <nav className="nav">
           <img src="/logo-viajou-anotou-light.png" alt="Logo Viajou Anotou" className="logo" />
@@ -54,8 +12,81 @@ const App = () => {
           </ul>
         </nav>
       </header>
+      <main className="main-home">
+        <section>
+          <h1>Você viaja o mundo. <br />E o ViajouAnotou mantém suas aventuras anotadas.</h1>
+          <h2>Um mapa mundial que rastreia por onde você passou. Nunca esqueça suas experiências e mostre aos seus amigos o quê você fez pelo mundo.</h2>
+          <a className="cta" href="">Começar agora</a>
+        </section>
+      </main>
+    </>
+  )
+}
+
+const Sobre = () => {
+  return (
+    <>
+      <header>
+        <nav className="nav">
+          <img src="/logo-viajou-anotou-light.png" alt="Logo Viajou Anotou" className="logo" />
+          <ul>
+            <li><a href="">INÍCIO</a></li>
+            <li><a href="">PREÇO</a></li>
+            <li><a href="">SOBRE</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main className="main-about">
+        <section>
+          <div>
+            <h1>Sobre o ViajouAnotou.</h1>
+            <p>O ViajouAnotou nasceu do desejo dos amigos Paulo e Roberto de compartilharem de forma rápida suas aventuras pelo mundo.</p>
+            <p>Aos poucos, esse desejo virou realidade em forma de software entre amigos e familiares. Hoje, você também pode ser parte dessa comunidade.</p>
+          </div>
+          <img src="sobre-viajou-anotou.jpg" alt="Quatro pessoas sentadas admirando uma paisagem" />
+        </section>
+      </main>
+    </>
+  )
+}
+
+const Preco = () => {
+  return (
+    <div>
+      <header>
+        <nav className="nav">
+          <img src="/logo-viajou-anotou-light.png" alt="Logo Viajou Anotou" className="logo" />
+          <ul>
+            <li><a href="">INÍCIO</a></li>
+            <li><a href="">PREÇO</a></li>
+            <li><a href="">SOBRE</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main className="main-pricing">
+        <section>
+          <div>
+            <h1>Preço simples.<br />Só R$ 47/mês.</h1>
+            <p>Comece hoje mesmo a anotar suas aventuras e mostre aos seus amigos o quê você fez pelo mundo.</p>
+          </div>
+          <img src="preco-viajou-anotou.jpg" alt="Pessoas transitando em uma faixa de pedestre" />
+        </section>
+      </main>
     </div>
   )
 }
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/preco" element={<Preco />} />
+    </Route>
+  )
+)
+
+
+const App = () => <RouterProvider router={router} />
 
 export { App }
