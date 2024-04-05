cd0e04f14ec51c2c2045623eaa94ace6389e6e61
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-05 01:36:53 -0300

    ✔  Marcação pagina preço

--- src/app.jsx
+++ src/app.jsx
@@ -13,11 +13,21 @@ const App = () => {
       </header>
 
 
-      <main className="main-home">
+      {/* <main className="main-home">
         <section>
           <h1>Você viaja o mundo. <br />E o ViajouAnotou mantém suas aventuras anotadas.</h1>
           <h2>Um mapa mundial que rastreia por onde você passou. Nunca esqueça suas experiências e mostre aos seus amigos o quê você fez pelo mundo.</h2>
           <a className="cta" href="">Começar agora</a>
+        </section>
+      </main> */}
+
+      <main className="main-pricing">
+        <section>
+          <div>
+            <h1>Preço simples.<br />Só R$ 47/mês.</h1>
+            <p>Comece hoje mesmo a anotar suas aventuras e mostre aos seus amigos o quê você fez pelo mundo.</p>
+          </div>
+          <img src="preco-viajou-anotou.jpg" alt="Pessoas transitando em uma faixa de pedestre" />
         </section>
       </main>
 
