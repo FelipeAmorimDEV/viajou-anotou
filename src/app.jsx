7a79c10e815e7c3f808ec661f94e19010bbd3937
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-05 01:32:28 -0300

    ✔  Marcação da home

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,28 @@
 const App = () => {
- return <h1>Vite + React Boilerplate</h1>
+  return (
+    <div className="app">
+      <header>
+        <nav className="nav">
+          <img src="/logo-viajou-anotou-light.png" alt="Logo Viajou Anotou" className="logo" />
+          <ul>
+            <li><a href="">INÍCIO</a></li>
+            <li><a href="">PREÇO</a></li>
+            <li><a href="">SOBRE</a></li>
+          </ul>
+        </nav>
+      </header>
+
+
+      <main className="main-home">
+        <section>
+          <h1>Você viaja o mundo. <br />E o ViajouAnotou mantém suas aventuras anotadas.</h1>
+          <h2>Um mapa mundial que rastreia por onde você passou. Nunca esqueça suas experiências e mostre aos seus amigos o quê você fez pelo mundo.</h2>
+          <a className="cta" href="">Começar agora</a>
+        </section>
+      </main>
+
+    </div>
+  )
 }
 
 export { App }
