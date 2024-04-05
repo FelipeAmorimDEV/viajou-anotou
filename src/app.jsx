9d3a2c726f070b8f4a9993f6d9013d1888b47dd6
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-05 01:44:47 -0300

    ✔  Componente Home Criado

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,16 @@
+
+const Home = () => {
+  return (
+    <main className="main-home">
+      <section>
+        <h1>Você viaja o mundo. <br />E o ViajouAnotou mantém suas aventuras anotadas.</h1>
+        <h2>Um mapa mundial que rastreia por onde você passou. Nunca esqueça suas experiências e mostre aos seus amigos o quê você fez pelo mundo.</h2>
+        <a className="cta" href="">Começar agora</a>
+      </section>
+    </main>
+  )
+}
+
 const App = () => {
   return (
     <div className="app">
@@ -13,34 +26,7 @@ const App = () => {
       </header>
 
 
-      {/* <main className="main-home">
-        <section>
-          <h1>Você viaja o mundo. <br />E o ViajouAnotou mantém suas aventuras anotadas.</h1>
-          <h2>Um mapa mundial que rastreia por onde você passou. Nunca esqueça suas experiências e mostre aos seus amigos o quê você fez pelo mundo.</h2>
-          <a className="cta" href="">Começar agora</a>
-        </section>
-      </main> */}
 
-      {/* <main className="main-pricing">
-        <section>
-          <div>
-            <h1>Preço simples.<br />Só R$ 47/mês.</h1>
-            <p>Comece hoje mesmo a anotar suas aventuras e mostre aos seus amigos o quê você fez pelo mundo.</p>
-          </div>
-          <img src="preco-viajou-anotou.jpg" alt="Pessoas transitando em uma faixa de pedestre" />
-        </section>
-      </main> */}
-
-      <main className="main-pricing">
-        <section>
-          <img src="sobre-viajou-anotou.jpg" alt="Quatro pessoas sentadas admirando uma paisagem" />
-          <div>
-            <h1>Sobre o ViajouAnotou.</h1>
-            <p>O ViajouAnotou nasceu do desejo dos amigos Paulo e Roberto de compartilharem de forma rápida suas aventuras pelo mundo.</p>
-            <p>Aos poucos, esse desejo virou realidade em forma de software entre amigos e familiares. Hoje, você também pode ser parte dessa comunidade.</p>
-          </div>
-        </section>
-      </main>
 
     </div>
   )
