ec79768e532b5977ee82f90853cc1157cd9ae679
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-05 01:46:30 -0300

    ✔  Componente Sobre criado

--- src/app.jsx
+++ src/app.jsx
@@ -11,6 +11,21 @@ const Home = () => {
   )
 }
 
+const Sobre = () => {
+  return (
+    <main className="main-about">
+    <section>
+      <div>
+        <h1>Sobre o ViajouAnotou.</h1>
+        <p>O ViajouAnotou nasceu do desejo dos amigos Paulo e Roberto de compartilharem de forma rápida suas aventuras pelo mundo.</p>
+        <p>Aos poucos, esse desejo virou realidade em forma de software entre amigos e familiares. Hoje, você também pode ser parte dessa comunidade.</p>
+      </div>
+      <img src="sobre-viajou-anotou.jpg" alt="Quatro pessoas sentadas admirando uma paisagem" />
+    </section>
+  </main>
+  )
+}
+
 const App = () => {
   return (
     <div className="app">
@@ -24,10 +39,6 @@ const App = () => {
           </ul>
         </nav>
       </header>
-
-
-
-
     </div>
   )
 }
